ec1313abf815c77c450e76be0b34fd53a5ff5484
ph <patrick@heisiph.de>
2012-09-17 12:32:57 +0800

    update to use most current release of wysihtml5

--- app/assets/javascripts/active_admin/editor.js
+++ app/assets/javascripts/active_admin/editor.js
@@ -1,4 +1,4 @@
-//= require active_admin/editor/wysihtml5.min
+//= require active_admin/editor/wysihtml5-0.3.0.min
 //= require active_admin/editor/wysihtml5/parser_rules
 //= require active_admin/editor/fileuploader
 //= require active_admin/editor/jquery.paginate.min
